1cd605052d46efab067ffa33cf21b82c030195f0
rinkuvatscs <rinkuvatscs@gmail.com>
2017-09-23 21:51:09 +0530

    Info Version One component completed

---
+++ src/main/java/com/woocation/ui/mapper/request/InfoTextVersionOneRequest.java
@@ -0,0 +1,31 @@
+package com.woocation.ui.mapper.request;
+
+public class InfoTextVersionOneRequest {
+
+	private String order ;
+	
+	private String textSectionText ;
+	private String valueSectionText;
+	
+	public String getOrder() {
+		return order;
+	}
+	public void setOrder(String order) {
+		this.order = order;
+	}
+	public String getTextSectionText() {
+		return textSectionText;
+	}
+	public void setTextSectionText(String textSectionText) {
+		this.textSectionText = textSectionText;
+	}
+	public String getValueSectionText() {
+		return valueSectionText;
+	}
+	public void setValueSectionText(String valueSectionText) {
+		this.valueSectionText = valueSectionText;
+	} 
+	
+	
+	
+}
---
+++ src/main/java/com/woocation/ui/mapper/service/impl/InfoVersionOneWoocationComponent.java
@@ -0,0 +1,114 @@
+package com.woocation.ui.mapper.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import com.woocation.ui.mapper.request.ApplicationVars;
+import com.woocation.ui.mapper.request.InfoTextVersionOneRequest;
+import com.woocation.ui.mapper.response.WoocationCompoonentResponse;
+import com.woocation.ui.mapper.service.WoocationComponent;
+import static com.woocation.ui.mapper.constants.WoocationTypes.*;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Component
+public class InfoVersionOneWoocationComponent implements WoocationComponent {
+
+	@Autowired
+	private ApplicationVarsServiceImpl applicationVarsServiceImpl;
+
+	@Value("${enable.applications.vars:false}")
+	private boolean isVarsEnable;
+
+	public WoocationCompoonentResponse processRequest(InfoTextVersionOneRequest infoTextVersionOneRequest) {
+		WoocationCompoonentResponse woocationCompoonentResponse = new WoocationCompoonentResponse();
+		woocationCompoonentResponse.getHeadingResponse().put(WOOCATION_ORDER.getMessage(),
+				infoTextVersionOneRequest.getOrder());
+		if (isVarsEnable) {
+			processForGlobalVars(woocationCompoonentResponse);
+			processForComponentVars(woocationCompoonentResponse);
+		} else {
+			componentDefaultConfiguraions(woocationCompoonentResponse, infoTextVersionOneRequest);
+		}
+		return woocationCompoonentResponse;
+	}
+
+	private void processForComponentVars(WoocationCompoonentResponse headingCompoonentResponse) {
+		ApplicationVars componentHeadingApplicationVars = applicationVarsServiceImpl
+				.getApplicationVars(WOOCATION_INFO_TEXT_ONE_COMPONENT_NAME.getMessage());
+		if (!StringUtils.isEmpty(componentHeadingApplicationVars)
+				&& !StringUtils.isEmpty(componentHeadingApplicationVars.getApplicationVarsContent())) {
+			componentHeadingApplicationVars.getApplicationVarsContent()
+					.forEach((key, value) -> headingCompoonentResponse.getHeadingResponse().put(key, value));
+		}
+	}
+
+	private void processForGlobalVars(WoocationCompoonentResponse headingCompoonentResponse) {
+
+		ApplicationVars globalHeadingApplicationVars = applicationVarsServiceImpl
+				.getApplicationVars(WOOCATION_GLOBAL_NAME.getMessage());
+		if (!StringUtils.isEmpty(globalHeadingApplicationVars)
+				&& !StringUtils.isEmpty(globalHeadingApplicationVars.getApplicationVarsContent())) {
+			headingCompoonentResponse.getHeadingResponse().put(WOOCATION_PADDING.getMessage(),
+					globalHeadingApplicationVars.getApplicationVarsContent().get(WOOCATION_PADDING.getMessage()));
+		}
+	}
+
+	private void componentDefaultConfiguraions(WoocationCompoonentResponse headingCompoonentResponse,
+			InfoTextVersionOneRequest infoTextVersionOneRequest) {
+
+		headingCompoonentResponse.getHeadingResponse().put(WOOCATION_DISPLAY_NAME.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_DISPLAY_NAME_DEFAULT_VALUE.getMessage());
+		headingCompoonentResponse.getHeadingResponse().put(WOOCATION_TYPE.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_TYPE_DEFAULT_VALUE.getMessage());
+		headingCompoonentResponse.getHeadingResponse().put(WOOCATION_BG_COLOR.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_BG_COLOR_DEFAULT_VALUE.getMessage());
+		headingCompoonentResponse.getHeadingResponse().put(WOOCATION_WIDTH.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_WIDTH_DEFAULT_VALUE.getMessage());
+		headingCompoonentResponse.getHeadingResponse().put(WOOCATION_PADDING.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_PADDING_DEFAULT_VALUE.getMessage());
+
+		headingCompoonentResponse.getHeadingResponse().put(WOOCATION_TEXT_SECTION.getMessage(),
+				processTextSection(infoTextVersionOneRequest));
+		headingCompoonentResponse.getHeadingResponse().put(WOOCATION_VALUE_SECTION.getMessage(),
+				processValueSection(infoTextVersionOneRequest));
+
+	}
+
+	private Map<String, Object> processTextSection(InfoTextVersionOneRequest infoTextVersionOneRequest) {
+		Map<String, Object> textMap = Collections.emptyMap();
+		textMap.put(WOOCATION_TEXT.getMessage(), infoTextVersionOneRequest.getTextSectionText());
+		textMap.put(WOOCATION_WIDTH.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_TEXT_SECTION_WIDTH_DEFAULT_VALUE.getMessage());
+		textMap.put(WOOCATION_WIDTH.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_TEXT_SECTION_FONT_COLOR_DEFAULT_VALUE.getMessage());
+		textMap.put(WOOCATION_WIDTH.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_TEXT_SECTION_FONT_SIZE_DEFAULT_VALUE.getMessage());
+		textMap.put(WOOCATION_WIDTH.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_TEXT_SECTION_FONT_STYLE_DEFAULT_VALUE.getMessage());
+
+		return textMap;
+	}
+
+	private Map<String, Object> processValueSection(InfoTextVersionOneRequest infoTextVersionOneRequest) {
+		Map<String, Object> valueMap = Collections.emptyMap();
+		valueMap.put(WOOCATION_TEXT.getMessage(), infoTextVersionOneRequest.getValueSectionText());
+		valueMap.put(WOOCATION_WIDTH.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_VALUE_SECTION_WIDTH_DEFAULT_VALUE.getMessage());
+		valueMap.put(WOOCATION_ALIGN_ITEMS.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_VALUE_SECTION_ALIGN_ITEMS_DEFAULT_VALUE.getMessage());
+		valueMap.put(WOOCATION_TYPE.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_VALUE_SECTION_VALUE_TYPE_DEFAULT_VALUE.getMessage());
+		valueMap.put(WOOCATION_FONT_COLOR.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_VALUE_SECTION_FONT_COLOR_DEFAULT_VALUE.getMessage());
+		valueMap.put(WOOCATION_FONT_SIZE.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_VALUE_SECTION_FONT_SIZE_DEFAULT_VALUE.getMessage());
+		valueMap.put(WOOCATION_FONT_STYLE.getMessage(),
+				WOOCATION_INFO_TEXT_ONE_VALUE_SECTION_FONT_STYLE_DEFAULT_VALUE.getMessage());
+		return valueMap;
+	}
+
+}
